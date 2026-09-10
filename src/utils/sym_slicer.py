98b3244ec05323fb8e179da867281d1bda913ea0
GentleMin <minjingtao@gmail.com>
2026-09-10 15:43:58 +0200

    Function to slice hydro sym operators from MHD matrix

--- src/utils/sym_slicer.py
+++ src/utils/sym_slicer.py
@@ -113,3 +113,33 @@ def slice_Hydro_from_MHD(A, N, dL, perm=None):
     A = A[np.ix_(idx_perm, idx_perm)]
     perm = perm[np.ix_(idx_perm, idx)]
     return A, perm
+
+
+def slice_Hydro_sym_from_MHD(A, N, dL, sym_v=0, perm=None):
+
+    A = sparse.csr_array(A)
+    Nd = N*dL
+    # assert A.shape[0] == 4*Nd + Ntrail
+
+    idx_v_tor = idx_sym_torpol(N, dL, sym=sym_v, type_='t')
+    idx_v_pol = idx_sym_torpol(N, dL, sym=sym_v, type_='p')
+    idx = np.r_[idx_v_tor, Nd + idx_v_pol]
+
+    # Tau variables
+    itau_v_t = np.arange(dL)[((sym_v+1)%2)::2]
+    itau_v_p = np.arange(dL)[((sym_v+0)%2)::2]
+    idx = np.r_[idx, 
+        4*Nd + 0*dL + itau_v_t, 4*Nd + 1*dL + itau_v_t,
+        4*Nd + 2*dL + itau_v_p, 4*Nd + 3*dL + itau_v_p, 4*Nd + 4*dL + itau_v_p, 4*Nd + 5*dL + itau_v_p,
+    ]
+
+    if perm is None:
+        perm = sparse.eye_array(A.shape[0], format='csr')
+    
+    ele_arr = np.zeros(A.shape[0])
+    ele_arr[idx] = 1
+    idx_perm = np.arange(A.shape[0])[np.asarray(perm @ ele_arr, dtype=bool)]
+
+    A = A[np.ix_(idx_perm, idx_perm)]
+    perm = perm[np.ix_(idx_perm, idx)]
+    return A, perm
